7f3b02ec1bac10378d9304810e895e385712356f
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-04-30 23:43:06 +0530

    feat: socket.io logic

--- chat app/server.js
+++ chat app/server.js
@@ -1,7 +1,25 @@
 const port = 4500;
 const io = require('socket.io')(port);
 
-io.on('connection',socket =>{
+const users = {};
+
+io.on('connection', socket => {
     console.log('new user')
     socket.emit('chat-message', 'Hello World');
+
+    socket.on('send-chat-message', message => {
+        //console.log(message);
+        socket.broadcast.emit('chat-message', { name: users[socket.id], message: message });
+    });
+
+    socket.on('disconnect', message => {
+        //console.log(message);
+        socket.broadcast.emit('user-disconnected',  users[socket.id]);
+        delete users[socket.id];
+    });
+
+    socket.on('new-user', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('new-user', name);
+    })
 })
