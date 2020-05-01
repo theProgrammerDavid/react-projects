d95faf7e5f5ce3c19b8659ddfd145bb3ef1148db
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-01 13:21:32 +0530

    feat: [WIP] ReactJs Integration

--- chat app/server.js
+++ chat app/server.js
@@ -1,25 +1,17 @@
-const port = 4500;
-const io = require('socket.io')(port);
+const io = require('socket.io')(3000)
 
-const users = {};
+const users = {}
 
 io.on('connection', socket => {
-    console.log('new user')
-    socket.emit('chat-message', 'Hello World');
-
-    socket.on('send-chat-message', message => {
-        //console.log(message);
-        socket.broadcast.emit('chat-message', { name: users[socket.id], message: message });
-    });
-
-    socket.on('disconnect', message => {
-        //console.log(message);
-        socket.broadcast.emit('user-disconnected',  users[socket.id]);
-        delete users[socket.id];
-    });
-
     socket.on('new-user', name => {
-        users[socket.id] = name;
-        socket.broadcast.emit('new-user', name);
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected', name)
+    })
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+    })
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
     })
 })
