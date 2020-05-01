07112b07be0fecdb2d93437e65a47996c111e8c3
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-05-01 19:03:53 +0530

    feat: exit button

--- chat app/script.js
+++ chat app/script.js
@@ -2,8 +2,8 @@ const socket = io('http://localhost:3000')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
-
-const name = prompt('What is your name?')
+const name='dav';
+//const name = prompt('What is your name?')
 appendMessage('You joined')
 socket.emit('new-user', name)
 
