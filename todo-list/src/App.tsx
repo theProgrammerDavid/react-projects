f076c135d9dc4ed2fe9d3d0f0ada778b82524ad8
theProgrammerDavid <davidvelho2awesome@gmail.com>
2020-11-14 22:31:41 +0530

    feat: material ui, add delete todo

--- todo-list/src/App.tsx
+++ todo-list/src/App.tsx
@@ -1,7 +1,8 @@
-import React, { ChangeEvent, MouseEvent } from 'react';
+import React, { ChangeEvent } from 'react';
 import Todos from './todos';
 import './App.css';
 import Navbar from './NavBar';
+import { Container, TextField, Button } from '@material-ui/core'
 
 class App extends React.Component {
 
@@ -15,7 +16,10 @@ class App extends React.Component {
   };
 
   deleteTodo = (id: number) => {
-    console.log('delete todo ');
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    })
+    this.setState({ todos: todos });
   }
 
   onTodoChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -26,11 +30,16 @@ class App extends React.Component {
 
   addNewTodo = () => {
     this.state.currentIndex++;
+   
     this.state.todos.push({
       id: this.state.currentIndex, content: this.state.newTodoValue
     });
-    // this.forceUpdate();
-    this.setState(this.state)
+    // const newArray = this.state.todos.push({id: this.state.currentIndex, content: this.state.newTodoValue})
+    // this.setState({
+    //   todos: newArray
+    // })
+    this.forceUpdate();
+    // this.setState(this.state)
 
   }
 
@@ -38,10 +47,16 @@ class App extends React.Component {
     return (
       <>
         <Navbar></Navbar>
-
-        <input type="text" placeholder="new todo" value={this.state.newTodoValue} onChange={this.onTodoChange}></input>
-        <button onClick={this.addNewTodo}>Add</button>
-        <Todos todos={this.state.todos} cb={this.deleteTodo}></Todos>
+        <br />
+        <Container>
+          <TextField id="filled-basic" label="Add New Todo" variant="filled" value={this.state.newTodoValue} onChange={this.onTodoChange} />
+          <Button variant="contained" color="primary" onClick={this.addNewTodo}>
+            Primary
+          </Button>
+          {/* <input type="text" placeholder="new todo" value={this.state.newTodoValue} onChange={this.onTodoChange}></input> */}
+          {/* <button onClick={this.addNewTodo}>Add</button> */}
+          <Todos todos={this.state.todos} cb={this.deleteTodo}></Todos>
+        </Container>
       </>
     )
   }
--- todo-list/src/todos.tsx
+++ todo-list/src/todos.tsx
@@ -1,34 +1,72 @@
 import React from 'react';
-
-
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import ListSubheader from '@material-ui/core/ListSubheader';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import SendIcon from '@material-ui/icons/Send';
+import { Container } from '@material-ui/core'
 
 interface Todo {
     todos: { id: number; content: string; }[];
     cb: (id: number) => void;
 
 }
- 
-
-
-// const Todos = (todo: Todo, todoDeleteFn: (id: number) => int,) => {
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+            width: '100%',
+            maxWidth: 360,
+            backgroundColor: theme.palette.background.paper,
+        },
+        nested: {
+            paddingLeft: theme.spacing(4),
+        },
+    }),
+);
 const Todos: React.FC<Todo> = (todo) => {
-    const { todos } = todo;
+    const classes = useStyles();
+    const { todos, cb } = todo;
 
     const todoList = todos.length ? (
         todos.map(((todo) => {
             return (
                 <div key={todo.id}>
-                    <p> <span onClick={() => {  }}>{todo.content}</span></p>
+
+                    <ListItem button >
+                        <ListItemIcon>
+                            <SendIcon />
+                        </ListItemIcon>
+                        <ListItemText primary={todo.content} />
+                    </ListItem>
                 </div>
+                // <div key={todo.id}> 
+                //     <ListItem button>
+                //         <ListItemText primary={todo.content}></ListItemText>
+                //     <span onClick={() => { cb(todo.id) }}>{todo.content}</span>
+                //     </ListItem>
+                // </div>
             )
         }))
     ) : (<p>You have no more todos left</p>);
 
     return (
-        <div>
-            {todoList}
 
-        </div>
+        <List
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+            subheader={
+                <ListSubheader component="div" id="nested-list-subheader">
+                    Nested List Items
+                    </ListSubheader>
+            }
+            className={classes.root}
+        >
+
+            {todoList}
+        </List>
+
 
     )
 }
